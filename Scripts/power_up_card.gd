698f5d98866e08828f9420c5e977a555fd8c8087
lvl5BeanWizard <raycockerham1994@gmail.com>
2026-04-04 16:24:14 -0400

    powerups return their type when picked

--- Scripts/power_up_card.gd
+++ Scripts/power_up_card.gd
@@ -6,17 +6,22 @@ var power_desc
 
 @export var powerup: Powerup
 
-@onready var name_txt = $TextureRect/Name
-@onready var desc_txt = $TextureRect/Descr
-@onready var image = $TextureRect/Image
+@onready var name_txt = $Button/Name
+@onready var desc_txt = $Button/Descr
+@onready var image = $Button/Image
+
+signal powerup_picked(selected_powerup: Powerup)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	name_txt.text = powerup.name
 	desc_txt.text = powerup.desc
 	image.texture = powerup.texture
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_button_pressed():
+	#on click, send out a signal saying we picked this one
+	powerup_picked.emit(powerup)
