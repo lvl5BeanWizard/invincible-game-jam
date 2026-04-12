6e81bc5f6db62f66d5e918a62a05b448a59e7be9
lvl5BeanWizard <raycockerham1994@gmail.com>
2026-04-12 13:29:29 -0400

    splosions

--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -2,9 +2,23 @@ extends Node3D
 
 var str: int = 3
 
+@onready var smoke = $Smoke
+@onready var fire = $Fire
+@onready var debris = $Debris
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(0.5).timeout
+	debris.emitting = true
+	smoke.emitting = true
+	fire.emitting = true
+	
+	#turn off the damage after 3/4s of a second
+	await get_tree().create_timer(0.75).timeout
+	$Area3D.monitorable = false
+	
+	#and delete itself after a full 2 seconds
+	await get_tree().create_timer(1.25).timeout
 	queue_free()
 
 func set_str(new_str: int):
